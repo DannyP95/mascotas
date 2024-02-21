57af9c4edc490a5ad8892c0a3bdecb21cc800183
DannyP95 <palaudanny@gmail.com>
2024-02-21 17:10:50 -0300

    agregado nav y puliendolo

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
-const path = require("path")  
+const express = require('express');
+const path = require('path');
 
-const express = require('express')  
+const app = express();
 
-const app = express()  
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static("public"))  
+app.use(express.json());
 
-app.listen(3030,() => console.log('Servidor corriendo en el puerto 3030')) 
+//template engine
+app.set('view engine', 'ejs'); // establece ejs como el modelo de plantillas 
+app.set('views', path.join(__dirname, 'src/views')); // le deci a ejs que las plantillas estan en la carpeta views
 
-app.get('/', (req, res) => {          
-    console.log(__dirname);     
-    res.sendFile(path.resolve(__dirname, "./views/index.html"))  
-})  
+// Requeriendo Rutas 
+const mainRutas = require('./src/routes/main')
 
-app.get('/babbage', (req, res) => {          
-    console.log(__dirname);     
-    res.sendFile(path.resolve(__dirname, "./views/babbage.html"))  
-})  
+// Implementando Rutas 
+app.use('/', mainRutas);
 
-app.get('/clarke', (req, res) => {          
-    console.log(__dirname);     
-    res.sendFile(path.resolve(__dirname, "./views/clarke.html"))  
+// Error 404
+
+app.use((req,res) => {
+    res.status(404).render('error')
 })
+
+
+
+// Escuchando Servidor 
+app.listen(3003,() => console.log('Servidor corriendo en el puerto 3003')) 
